d354068683f30ce1819fd46c27ff5726de85db98
Bekzod <bekzodturgunoff@gmail.com>
2022-08-22 17:15:34 +0500

    scss added. Navbar style ->  desktop and monile done.

--- src/Container/Navbar/Navbar.jsx
+++ src/Container/Navbar/Navbar.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
-import "./Navbar.css"
+import "./Navbar.scss"
 import { Images } from '../../Constants'
 
 function Navbar() {
   return (
 <div className='navbar-app'>
     <div className='navbar'>
+
         <div className='logo'><img src={Images.bookmark} alt="logo" /></div>
+<div className='hamburger-links'>
+<img src={Images.iconHamburger} alt="hamburger" />
+</div>
+        <div className='links'>
+            <ul>
+                <li><a href="#">Features</a></li>
+                <li><a href="#">Pricing</a></li>
+                <li><a href="#">Contact</a></li>
+            <button>Clikc</button>
+
+            </ul>
+
+        </div>
+        
+
     </div>
 </div>
     )
