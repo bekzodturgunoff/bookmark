fc30d05971ac7eab3b67700962143a032ddd3653
jcbbb <avaz28082000@gmail.com>
2022-08-23 10:05:51 +0500

    fixed

--- src/Container/Features/Features.jsx
+++ src/Container/Features/Features.jsx
@@ -1,109 +1,71 @@
-import React, { useReducer } from 'react'
-import "./Features.scss"
-import { Images } from '../../Constants';
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "TAB_ONE":
-      return { openTabOne: !state.showText };
-    case "TAB_TWO":
-      return { openTabTwo: !state.showText };
-    case "TAB_THREE":
-      return { openTabThree: !state.showText };
-    default:
-      state;
-  }
-};
+import * as React from "react";
+import "./Features.scss";
+import { Images } from "../../Constants";
 
 function Features() {
-
-  const [state, dispatch] = useReducer(reducer, { openTabOne: true, openTabTwo: false, openTabThree: false });
+  const [activeTab, setActiveTab] = React.useState(1);
 
   return (
-    <div className='features-app'>
-        <div className='features'>
-          <h1>Features</h1>
-          <p>Our aim is to make it quick and easy for you to access your favourite 
-            websites. Your bookmark sycns between your devices so you can access them 
-            on the go.
-          </p>
-        </div>
-        <div className='features-tabs'>
-          <div onClick={() => {
-          dispatch({ type: "TAB_ONE" });
-        }}
-      >
-        <h3>
-        Simple Bookmarking
-        </h3>
-        <div className='tab-one'>
-        {state.openTabOne && <div className='tab-one-container'>
-          <div className='tab-one-img'>
-            <img src={Images.featuresTabOne} alt="tab-img" />
-          </div>
-          <div className='tab-one-text'>
-            <h1>Bookmark in one click</h1>
-            <p>Orgonize your bookmarks however you like. Our simple drag-and-drop
-                interfacce gives you complete control over how you manage your favorite
-                sites.
-            </p>
-            <button>More info</button>
-          </div>
-           </div>
-           
-           }
-        
-        </div>
+    <div class="tabs-container">
+      <div class="header">
+        <h1>Features</h1>
+        <p>
+          Our aim is to make it quick and easy for you to access your favourite websites. Your
+          bookmark sycns between your devices so you can access them on the go.
+        </p>
       </div>
-          <div  onClick={() => {
-          dispatch({ type: "TAB_TWO" });
-        }}>
-          <h3>
-          Speedy Searching
-          </h3>
-          <div className='tab-two'>
-          {state.openTabTwo && <div className='tab-two-container'>
-          <div className='tab-two-img'>
-            <img src={Images.featuresTabOne} alt="tab-img" />
-          </div>
-          <div className='tab-two-text'>
-            <h1>Intelligent search </h1>
-            <p>Our powerful search feature will help you find saved
-              ites in no time at all. No need to trawl through all of
-              your bookmarks.
+      <ul className="tabs">
+        <li>
+          <button onClick={() => setActiveTab(1)}>Simple Bookmarking</button>
+        </li>
+        <li>
+          <button onClick={() => setActiveTab(2)}>Speedy Searching</button>
+        </li>
+        <li>
+          <button onClick={() => setActiveTab(3)}>Easy Sharing</button>
+        </li>
+      </ul>
+      {activeTab === 1 ? (
+        <div className="feature-tab">
+          <img src={Images.featuresTabOne} alt="" />
+          <div>
+            <h2>Bookmark in one click</h2>
+            <p>
+              Orgonize your bookmarks however you like. Our simple drag-and-drop interfacce gives
+              you complete control over how you manage your favorite sites.
             </p>
             <button>More info</button>
           </div>
-           </div>}
-          
-          </div>
         </div>
-          <div  onClick={() => {
-          dispatch({ type: "TAB_THREE" });
-        }}>
-          <h3>
-          Easy Sharing
-          </h3>
-          <div className='tab-three'>
-          {state.openTabThree && <div className='tab-three-container'>
-          <div className='tab-three-img'>
-            <img src={Images.featuresTabOne} alt="tab-img" />
-          </div>
-          <div className='tab-three-text'>
-            <h1>Share your bookmarks</h1>
-            <p>Easily share your bookmarks and collections with
-              others. Create a shareable link that you can send at
-              the click of a button.
+      ) : null}
+      {activeTab === 2 ? (
+        <div className="feature-tab">
+          <img src={Images.featuresTabTwo} alt="" />
+          <div>
+            <h2>Intelligent Search</h2>
+            <p>
+              Our powerful search feature will help you find saved ites in no time at all. No need
+              to trawl through all of your bookmarks.
             </p>
             <button>More info</button>
           </div>
-           </div>}
-          
+        </div>
+      ) : null}
+      {activeTab === 3 ? (
+        <div className="feature-tab">
+          <img src={Images.featuresTabThree} alt="" />
+          <div>
+            <h2>Share your bookmarks</h2>
+            <p>
+              Easily share your bookmarks and collections with others. Create a shareable link that
+              you can send at the click of a button.
+            </p>
+            <button>More info</button>
           </div>
         </div>
-        </div>
+      ) : null}
     </div>
-  )
+  );
 }
 
-export default Features
+export default Features;
